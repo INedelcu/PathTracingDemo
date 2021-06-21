f4844579c4178404041ca79fc325526c44cdb7d9
Kasper Engelstoft <kaspere@unity3d.com>
2021-06-21 13:53:39 +0200

    clean up dead code

--- Assets/Scripts/SRP/RaytracingRenderPipelineInstance.cs
+++ Assets/Scripts/SRP/RaytracingRenderPipelineInstance.cs
@@ -99,30 +99,8 @@ public class RaytracingRenderPipelineInstance : RenderPipeline
             if (renderPipelineAsset.rayTracingAccelerationStructure == null)
                 return;
             
-            // Get the culling parameters from the current Camera
-            //camera.TryGetCullingParameters(out var cullingParameters);
-
-            // Use the culling parameters to perform a cull operation, and store the results
-            //var cullingResults = context.Cull(ref cullingParameters);
-
             // Update the value of built-in shader variables, based on the current Camera
             context.SetupCameraProperties(camera);
-
-            // Tell Unity which geometry to draw, based on its LightMode Pass tag value
-            //ShaderTagId shaderTagId = new ShaderTagId("ExampleLightModeTag");
-
-            // Tell Unity how to sort the geometry, based on the current Camera
-            //var sortingSettings = new SortingSettings(camera);
-
-            // Create a DrawingSettings struct that describes which geometry to draw and how to draw it
-            //DrawingSettings drawingSettings = new DrawingSettings(shaderTagId, sortingSettings);
-
-            // Tell Unity how to filter the culling results, to further specify which geometry to draw
-            // Use FilteringSettings.defaultValue to specify no filtering
-            //FilteringSettings filteringSettings = FilteringSettings.defaultValue;
-        
-            // Schedule a command to draw the geometry, based on the settings you have defined
-            //context.DrawRenderers(cullingResults, ref drawingSettings, ref filteringSettings);
 
             // Schedule a command to draw the Skybox if required
             if (camera.clearFlags == CameraClearFlags.Skybox && RenderSettings.skybox != null)
