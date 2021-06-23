d7706be632c0d6a94eee2fa9a5626142962fcc5f
Ionut Nedelcu <ionut@unity3d.com>
2021-06-23 10:38:28 +0200

    Declared blue noise textures as global resources to improve CPU perf on the render thread.

--- Assets/Shaders/GlobalResources.hlsl
+++ Assets/Shaders/GlobalResources.hlsl
@@ -1,5 +1,16 @@
-RaytracingAccelerationStructure g_AccelStruct : register(t0, space1);
+#ifndef GLOBAL_RESOURCES_INCLUDED
+#define GLOBAL_RESOURCES_INCLUDED
 
-uint g_BounceCountOpaque;
-uint g_BounceCountTransparent;
+RaytracingAccelerationStructure g_AccelStruct           : register(t0, space1);
+Texture2D<float>                _ScramblingTileXSPP     : register(t1, space1);
+Texture2D<float>                _RankingTileXSPP        : register(t2, space1);
+Texture2D<float2>               _ScramblingTexture      : register(t3, space1);
+Texture2D<float2>               _OwenScrambledTexture   : register(t4, space1);
 
+cbuffer GlobalParams : register(b0, space1)
+{
+    uint g_BounceCountOpaque;
+    uint g_BounceCountTransparent;
+};
+
+#endif
