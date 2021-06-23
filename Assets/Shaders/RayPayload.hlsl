45ff95edb768541e71163fb061954a6e7e2d0e88
Arthur Dufay <arthur.dufay@unity3d.com>
2021-06-23 10:58:48 +0200

    Fix bounce count

--- Assets/Shaders/RayPayload.hlsl
+++ Assets/Shaders/RayPayload.hlsl
@@ -2,8 +2,8 @@ struct RayPayload
 {
     float3 albedo;
     float3 emission;
-    uint bounceIndexOpaque;
-    uint bounceIndexTransparent;
+    int bounceIndexOpaque;
+    int bounceIndexTransparent;
     float3 bounceRayOrigin;
     float3 bounceRayDirection;
     float3 lastWorldNormal;
